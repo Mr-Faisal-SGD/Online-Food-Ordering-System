d68f282bfb8354597148be6ba94e7a9318d52d42
Mr-Faisal-SGD <faisal450fai@gmail.com>
2023-04-19 20:39:21 +0500

    s

--- backend/src/router/user.router.ts
+++ backend/src/router/user.router.ts
@@ -2,7 +2,9 @@ import { Router } from "express";
 import { sample_users } from "../data";
 import jwt from "jsonwebtoken";
 import asyncHandler from "express-async-handler";
-import { UserModel } from "../models/user.model";
+import { User, UserModel } from "../models/user.model";
+import { HTTP_BAD_REQUEST } from "../constants/http_status";
+import bcrypt from "bcryptjs";
 
 const router = Router();
 
@@ -26,7 +28,35 @@ router.post(
     const user = await UserModel.findOne({ email, password });
 
     if (user) res.send(generateTokenResponse(user));
-    else res.status(400).send("Username or password is not valid");
+    else res.status(HTTP_BAD_REQUEST).send("Username or password is invalid");
+  })
+);
+
+router.post(
+  "/register",
+  asyncHandler(async (req, res) => {
+    const { name, email, password, address } = req.body;
+    const user = await UserModel.findOne({ email });
+    if (user) {
+      res
+        .status(HTTP_BAD_REQUEST)
+        .send("Email is already linked with other account");
+      return;
+    }
+
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    const newUser: User = {
+      id: "",
+      name,
+      password: encryptedPassword,
+      email: email.toLowerCase(),
+      address,
+      isAdmin: false,
+    };
+
+    const dbUser = await UserModel.create(newUser);
+    res.send(generateTokenResponse);
   })
 );
 
